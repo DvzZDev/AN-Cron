d04a78cb135b87efbe1baa4c4bbba4e007826381
DvzZ <estebandavid578@gmail.com>
2024-10-07 23:44:36 +0200

    Refactor code to use SupaBase

--- cuencas.js
+++ cuencas.js
@@ -1,78 +1,66 @@
-import mysql from 'mysql2';
-import axios from 'axios';
-import cheerio from 'cheerio';
-import { format } from 'date-fns';
-import unidecode from 'unidecode';
+import dotenv from "dotenv"
+import axios from "axios"
+import * as cheerio from "cheerio"
+import { format } from "date-fns"
+import unidecode from "unidecode"
+import { createClient } from "@supabase/supabase-js"
+
+dotenv.config()
+
 // Conexión a la base de datos
-const connection = mysql.createConnection({
-  host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
-  port: 4000,
-  user: 'DfWYA9vQa8C3KYP.root',
-  password: 'bITJL8OzxMkIstMi',
-  database: 'CuencasHidrograficas',
-  ssl: {
-    rejectUnauthorized: process.env.ssl_rejectUnauthorized,
-  }
-});
+const supabaseUrl = "https://rxxyplqherusqxdcowgh.supabase.co"
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 // URL de la página web que queremos scrappear
-const url = 'https://www.embalses.net/cuencas.php';
+const url = "https://www.embalses.net/cuencas.php"
 
 // Realizamos la petición a la web
-axios.get(url)
-  .then(response => {
+
+axios
+  .get(url)
+  .then((response) => {
     // Pasamos el contenido HTML de la web a un objeto Cheerio
-    const $ = cheerio.load(response.data);
+    const $ = cheerio.load(response.data)
 
     // Obtenemos la tabla donde están los datos
-    const table = $('table.Tabla');
+    const table = $("table.Tabla")
 
     // Recorremos todas las filas de la tabla para extraer los datos
-    $('tr.ResultadoCampo', table).each((_, row) => {
-      const columns = $('td', row);
-      const cuenca = unidecode($(columns[0]).text().trim().replace(' ', '_'));
-      const capacidad = $(columns[1]).text().trim();
-      const embalsada = $(columns[2]).text().trim();
-      const porcentaje_embalsada = parseFloat($(columns[3]).text().trim().replace('(', '').replace(')', '').replace('%', ''));
-      const variacion = $(columns[4]).text().trim();
-      const porcentaje_variacion = parseFloat($(columns[5]).text().trim().replace('(', '').replace(')', '').replace('%', ''));
-      const fecha_modificacion = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    $("tr.ResultadoCampo", table).each(async (_, row) => {
+      const columns = $("td", row)
+      const cuenca = unidecode($(columns[0]).text().trim().replace(" ", "_"))
+      const capacidad = $(columns[1]).text().trim()
+      const embalsada = $(columns[2]).text().trim()
+      const porcentaje_embalsada = parseFloat(
+        $(columns[3]).text().trim().replace("(", "").replace(")", "").replace("%", "")
+      )
+      const variacion = $(columns[4]).text().trim()
+      const porcentaje_variacion = parseFloat(
+        $(columns[5]).text().trim().replace("(", "").replace(")", "").replace("%", "")
+      )
+      const fecha_modificacion = format(new Date(), "yyyy-MM-dd HH:mm:ss")
 
-      // Verifica si la fila existe
-      connection.query(
-        `
-        INSERT INTO CUENCA (fecha_modificacion, cuenca, capacidad, embalsada, porcentaje_embalsada, variacion, porcentaje_variacion)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        ON DUPLICATE KEY UPDATE
-        fecha_modificacion = VALUES(fecha_modificacion),
-        capacidad = VALUES(capacidad),
-        embalsada = VALUES(embalsada),
-        porcentaje_embalsada = VALUES(porcentaje_embalsada),
-        variacion = VALUES(variacion),
-        porcentaje_variacion = VALUES(porcentaje_variacion)
-        `,
-        [fecha_modificacion, cuenca, capacidad, embalsada, porcentaje_embalsada, variacion, porcentaje_variacion],
-        (error, results) => {
-          if (error) {
-            console.error(`Error executing SQL query: ${error}`);
-          } else {
-            console.log(`Datos insertados o actualizados para la cuenca ${cuenca}`);
-          }
-        }
-      );
-    });
+      // Insertamos los datos en la base de datos
+      const { data, error } = await supabase.from("datos_cuencas").upsert([
+        {
+          cuenca,
+          capacidad,
+          embalsada,
+          porcentaje_embalsada,
+          variacion,
+          porcentaje_variacion,
+          fecha_modificacion,
+        },
+      ])
 
-    // Hacemos commit de la transacción
-    connection.commit((error) => {
       if (error) {
-        console.error(`Error committing transaction: ${error}`);
+        console.error("Error inserting data:", error)
       } else {
-        console.log('Transacción completada exitosamente');
+        console.log("Data inserted successfully:", data)
       }
-      connection.end();
-    });
+    })
   })
-  .catch(error => {
-    console.error(`Error fetching data from website: ${error}`);
-  });
-
+  .catch((error) => {
+    console.error("Error fetching data:", error)
+  })
--- datosEmbalses.js
+++ datosEmbalses.js
@@ -1,154 +1,151 @@
-import get from "axios";
-import { load } from "cheerio";
-import { createConnection } from "mysql2/promise";
+import dotenv from "dotenv"
+import axios from "axios"
+import * as cheerio from "cheerio"
+import { format } from "date-fns"
+import unidecode from "unidecode"
+import { createClient } from "@supabase/supabase-js"
 
-async function main() {
-  const fecha_modificacion = new Date().toISOString().slice(0, 19).replace("T", " ");
+dotenv.config()
 
-  const conn = await createConnection({
-    host: "gateway01.eu-central-1.prod.aws.tidbcloud.com",
-    port: 4000,
-    user: "DfWYA9vQa8C3KYP.root",
-    password: "bITJL8OzxMkIstMi",
-    database: "CuencasHidrograficas",
-    ssl: {
-      rejectUnauthorized: process.env.ssl_rejectUnauthorized,
-    },
-  });
+// Conexión a la base de datos
+const supabaseUrl = "https://rxxyplqherusqxdcowgh.supabase.co"
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-  const cursor = conn.connection;
+// URL de la página web que queremos scrappear
+const url = "https://www.embalses.net/cuencas.php"
 
-  const url = "https://www.embalses.net/cuencas.php";
+// Función para agregar un retraso
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-  const response = await get(url);
+// Realizamos la petición a la web
+axios
+  .get(url)
+  .then(async (response) => {
+    // Pasamos el contenido HTML de la web a un objeto Cheerio
+    const $ = cheerio.load(response.data)
 
-  const $ = load(response.data);
+    // Obtenemos la tabla donde están los datos
+    const table = $("table.Tabla")
 
-  const table = $("table.Tabla");
+    // Recorremos todas las filas de la tabla para extraer los datos
+    $("tr.ResultadoCampo", table).each(async (_, row) => {
+      const columns = $("td", row)
+      const cuenca = $(columns[0]).text().trim()
+      const cuenca_link = $(columns[0]).find("a").attr("href")
 
-  const rows = table.find("tr.ResultadoCampo");
+      await delay(1000) // Agregar un retraso de 1 segundo entre solicitudes
 
-  for (let i = 0; i < rows.length; i++) {
-    const row = $(rows[i]);
-    const columns = row.find("td");
-    const cuenca = $(columns[0]).text().trim();
-    const cuenca_link = $(columns[0]).find("a").attr("href");
+      try {
+        const cuenca_response = await axios.get(`https://www.embalses.net/${cuenca_link}`)
+        const cuenca_soup = cheerio.load(cuenca_response.data)
 
-    const cuenca_response = await get(`https://www.embalses.net/${cuenca_link}`);
-    const cuenca_soup = load(cuenca_response.data);
+        const cuenca_table = cuenca_soup("table.Tabla")
+        const cuenca_rows = cuenca_table.find("tr.ResultadoCampo")
 
-    const cuenca_table = cuenca_soup("table.Tabla");
-    const cuenca_rows = cuenca_table.find("tr.ResultadoCampo");
+        for (let j = 0; j < cuenca_rows.length; j++) {
+          const cuenca_row = $(cuenca_rows[j])
+          const cuenca_columns = cuenca_row.find("td")
+          if (cuenca_columns.length >= 3) {
+            let embalse = $(cuenca_columns[0]).text().trim()
+            embalse = unidecode(embalse.replace(" [+]", "").trim())
+            const embalse_link = $(cuenca_columns[0]).find("a").attr("href")
 
-    for (let j = 0; j < cuenca_rows.length; j++) {
-      const cuenca_row = $(cuenca_rows[j]);
-      const cuenca_columns = cuenca_row.find("td");
-      if (cuenca_columns.length >= 3) {
-        let embalse = $(cuenca_columns[0]).text().trim();
-        embalse = embalse.replace("Ã±", "ñ").replace(" [+]", "").trim();
-        const embalse_link = $(cuenca_columns[0]).find("a").attr("href");
+            console.log(`Embalse: ${embalse} | Cuenca: ${cuenca}`)
 
-        console.log(`Embalse: ${embalse} | Cuenca: ${cuenca}`);
+            await delay(1000) // Agregar un retraso de 1 segundo entre solicitudes
 
-        const embalse_response = await get(`https://www.embalses.net/${embalse_link}`);
-        const embalse_soup = load(embalse_response.data);
+            try {
+              const embalse_response = await axios.get(
+                `https://www.embalses.net/${embalse_link}`
+              )
+              const embalse_soup = cheerio.load(embalse_response.data)
 
-        const divs = embalse_soup("div.SeccionCentral_Caja");
+              const divs = embalse_soup("div.SeccionCentral_Caja")
 
-        if (divs.length < 2) {
-          console.log(
-            `No se encontraron al menos dos divs con la clase 'SeccionCentral_Caja' en el embalse ${embalse}`
-          );
-        } else {
-          const second_div = $(divs[1]);
+              if (divs.length < 2) {
+                console.log(
+                  `No se encontraron al menos dos divs con la clase 'SeccionCentral_Caja' en el embalse ${embalse}`
+                )
+              } else {
+                const second_div = $(divs[1])
 
-          const fila_seccion_divs = second_div.find("div.FilaSeccion");
+                const fila_seccion_divs = second_div.find("div.FilaSeccion")
 
-          for (let k = 0; k < fila_seccion_divs.length; k++) {
-            const fila_seccion_div = $(fila_seccion_divs[k]);
-            const campo_div = fila_seccion_div.find("div.Campo");
-            const resultado_divs = fila_seccion_div.find("div.Resultado");
-            const unidad_divs = fila_seccion_div.find("div.Unidad, div.Unidad2");
+                let agua_embalsada,
+                  agua_embalsada_por,
+                  variacion_ultima_semana,
+                  variacion_ultima_semana_por,
+                  capacidad_total,
+                  misma_semana_ultimo_año,
+                  misma_semana_ultimo_año_por,
+                  misma_semana_10años,
+                  misma_semana_10años_por
 
-            if (campo_div.length && resultado_divs.length && unidad_divs.length) {
-              const fila_datos = [];
-              for (let l = 0; l < resultado_divs.length; l++) {
-                const resultado = $(resultado_divs[l]).text().trim();
-                fila_datos.push(resultado);
+                fila_seccion_divs.each((k, fila_seccion_div) => {
+                  const fila_datos = []
+                  const resultado_divs = $(fila_seccion_div).find("div.Resultado")
+
+                  resultado_divs.each((_, resultado_div) => {
+                    const resultado = $(resultado_div).text().trim()
+                    fila_datos.push(resultado)
+                  })
+
+                  if (k === 0) {
+                    agua_embalsada = fila_datos[0].replace(".", "")
+                    agua_embalsada_por = fila_datos.length > 1 ? fila_datos[1] : null
+                  } else if (k === 1) {
+                    variacion_ultima_semana = fila_datos[0]
+                    variacion_ultima_semana_por =
+                      fila_datos.length > 1 ? fila_datos[1] : null
+                  } else if (k === 2) {
+                    capacidad_total = fila_datos[0].replace(".", "")
+                  } else if (k === 3) {
+                    misma_semana_ultimo_año = fila_datos[0]
+                    misma_semana_ultimo_año_por =
+                      fila_datos.length > 1 ? fila_datos[1] : null
+                  } else if (k === 4) {
+                    misma_semana_10años = fila_datos[0]
+                    misma_semana_10años_por = fila_datos.length > 1 ? fila_datos[1] : null
+                  }
+                })
+
+                const fecha_modificacion = format(new Date(), "yyyy-MM-dd HH:mm:ss")
+
+                // Insertamos los datos en la base de datos
+                const { data, error } = await supabase.from("datos_embalses").upsert([
+                  {
+                    fecha_modificacion,
+                    nombre_embalse: embalse,
+                    nombre_cuenca: cuenca,
+                    agua_embalsada,
+                    agua_embalsadapor: agua_embalsada_por,
+                    variacion_ultima_semana,
+                    variacion_ultima_semanapor: variacion_ultima_semana_por,
+                    capacidad_total,
+                    misma_semana_ultimo_año,
+                    misma_semana_ultimo_añopor: misma_semana_ultimo_año_por,
+                    misma_semana_10años,
+                    misma_semana_10añospor: misma_semana_10años_por,
+                  },
+                ])
+
+                if (error) {
+                  console.error("Error inserting data:", error)
+                } else {
+                  console.log("Data inserted successfully:", data)
+                }
               }
-
-              if (k === 0) {
-                var agua_embalsada = fila_datos[0].replace(".", "");
-                var agua_embalsada_por = fila_datos.length > 1 ? fila_datos[1] : null;
-              } else if (k === 1) {
-                var variacion_ultima_semana = fila_datos[0];
-                var variacion_ultima_semana_por =
-                  fila_datos.length > 1 ? fila_datos[1] : null;
-              } else if (k === 2) {
-                var capacidad_total = fila_datos[0].replace(".", "");
-              } else if (k === 3) {
-                var misma_semana_ultimo_año = fila_datos[0];
-                var misma_semana_ultimo_año_por =
-                  fila_datos.length > 1 ? fila_datos[1] : null;
-              } else if (k === 4) {
-                var misma_semana_10años = fila_datos[0];
-                var misma_semana_10años_por =
-                  fila_datos.length > 1 ? fila_datos[1] : null;
-              }
+            } catch (error) {
+              console.error(`Error fetching embalse data for ${embalse}:`, error)
             }
           }
-
-          const sql = `
-          INSERT INTO datos_embalses(
-            fecha_modificacion,
-            nombre_embalse,
-            nombre_cuenca,
-            agua_embalsada,
-            agua_embalsadapor,
-            variacion_ultima_semana,
-            variacion_ultima_semanapor,
-            capacidad_total,
-            misma_semana_ultimo_año,
-            misma_semana_ultimo_añopor,
-            misma_semana_10años,
-            misma_semana_10añospor
-          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-          ON DUPLICATE KEY UPDATE
-            fecha_modificacion = VALUES(fecha_modificacion),
-            agua_embalsada = VALUES(agua_embalsada),
-            agua_embalsadapor = VALUES(agua_embalsadapor),
-            variacion_ultima_semana = VALUES(variacion_ultima_semana),
-            variacion_ultima_semanapor = VALUES(variacion_ultima_semanapor),
-            capacidad_total = VALUES(capacidad_total),
-            misma_semana_ultimo_año = VALUES(misma_semana_ultimo_año),
-            misma_semana_ultimo_añopor = VALUES(misma_semana_ultimo_añopor),
-            misma_semana_10años = VALUES(misma_semana_10años),
-            misma_semana_10añospor = VALUES(misma_semana_10añospor)
-          `;
-
-          await cursor.execute(sql, [
-            fecha_modificacion,
-            embalse,
-            cuenca,
-            agua_embalsada,
-            agua_embalsada_por,
-            variacion_ultima_semana,
-            variacion_ultima_semana_por,
-            capacidad_total,
-            misma_semana_ultimo_año,
-            misma_semana_ultimo_año_por,
-            misma_semana_10años,
-            misma_semana_10años_por,
-          ]);
-
-          await conn.commit();
         }
+      } catch (error) {
+        console.error(`Error fetching cuenca data for ${cuenca}:`, error)
       }
-    }
-  }
-
-  await conn.end(); // Cerrando la conexión con la base de datos al finalizar.
-}
-
-// Llamada a la función principal
-main().catch(console.error);
+    })
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error)
+  })
